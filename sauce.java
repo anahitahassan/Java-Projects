ed898cce951758007f0f3eb211d67e74c6663288
anahitahassan <anahitarhassan@gmail.com>
2021-04-13 12:50:55 -0700

    added sauce file

---
+++ sauce.java
@@ -0,0 +1,90 @@
+
+public class sauce
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello world");
+
+		//variables without values;
+		int y;
+		double b; 
+
+		//variables created w/values;
+		int x = 7;
+		double GPA = 5.0;
+		String subject = "AP English";
+		boolean isCorrect = true;
+		
+		int a = 7;
+		System.out.println(a);
+
+		x = 11; 
+		System.out.println(x);
+
+		x = 8 + 9 / 3 - 4;
+		System.out.println(x);
+
+		int c = 6; 
+		x = x + c;
+		// x now changed to 13
+
+		int w = 9;
+		w = w / 2;
+		System.out.println(w);
+		// anytime there is a decimal, it will round DOWN
+		
+		double u = 9;
+		u = u / 2;
+		System.out.println(u);
+		//will be 4.5
+
+		System.out.print("Hello world");
+		System.out.println("Hello world");
+		//"Ln" tells it to skip a line
+
+
+		x = 7;
+		System.out.print("Hello world" + x);
+		//System.out.printLn(x);
+
+		// prints "Hello world 7"
+
+		String name = "Natalie";
+		System.out.print("Hello World, my name is " + name);
+		// prints "Hello world, my name is Natalie"
+
+
+
+
+		String myName = "Anahita";
+		int q = 16;
+		double shoesize = 8;
+		boolean isNotVegetarian = true;
+
+		//relationship rule
+	
+		//younger person
+		int younger = 2 * x + 7; 
+		System.out.println(younger);
+
+		//older person
+		int older = x / 2 - 7;
+		System.out.println(older);
+
+
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
