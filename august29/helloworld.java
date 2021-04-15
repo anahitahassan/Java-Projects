2c5fff05b6a585b6d78c3966764af888b0eba153
anahitahassan <anahitarhassan@gmail.com>
2021-04-15 16:16:05 -0700

    renamed class

--- august29/helloworld.java
+++ august29/helloworld.java
@@ -1,8 +1,10 @@
-//Anahita Hassan
-//Period 1
+// Anahita Hassan
+// Period 1
 
-public class august29 {
-  public static void main( String[] args ) {
-   System.out.println("Hello World!"); 
+public class helloworld 
+{
+    public static void main( String[] args ) 
+  {
+    System.out.println("Hello World!"); 
   }
 }
