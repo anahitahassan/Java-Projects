4764cea204a4de47a168259da30c53650b173ba6
anahitahassan <anahitarhassan@gmail.com>
2021-04-15 16:53:13 -0700

    updated json

---
+++ september5/project.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class project
+{
+  public static void main(String[] args)
+  {
+    System.out.println("     /\\");
+    System.out.println("    /  \\");
+    System.out.println("   /    \\");
+    System.out.println("  /      \\");
+    System.out.println("  --------");
+    System.out.println("  |      |");
+    System.out.println("  | \"AH\" |");
+    System.out.println(" /|      |\\");
+    System.out.println("/ |      | \\");
+    System.out.println("-------------");
+    System.out.println("   ||  ||");
+   
+    int x = 5;
+    int y = 6;
+    
+    System.out.println("sum:" + (y + x));
+    System.out.println("difference:" + (y - x));
+    System.out.println("product:" + y * x);
+    System.out.println("quotient:" + y / x);
+    System.out.println("remainder:" + y % x);
+  }
+}
