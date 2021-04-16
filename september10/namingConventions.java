6f320f8d23f862abf6475a45ce92fffa9d87ef69
anahitahassan <anahitarhassan@gmail.com>
2021-04-16 16:35:00 -0700

    minor edits

--- september10/namingConventions.java
+++ september10/namingConventions.java
@@ -2,15 +2,15 @@ public class namingConventions
 {
     public static void main(String[] args)
     {
-        // typical rules: 
+        // Typical rules: 
             // start with lowercase letter (ex. int xyz)
             // extra words start uppercase (ex. int smallTable, bigFatHuman)
             // this is konwn as camel case. 
             // no space allowed, so 2nd word uppercase is uppercase to be distinguished
-        // have to rules:
+        // Mandatory rules:
             // never start with number
             // allowed: letters, number, $, _ (ex. _tennis, _key_club$123)
-        // shortcuts (WILL BE ON QUIZ)
+        // Shortcuts (WILL BE ON QUIZ)
             // i=i+1 ===> i++; // only for one iteration. 
             // i=i+7 ===> i+=7;
             // i=i-7 ===> i-=7; also i*=7; i/=7; i%=7;
