7bd6eac9a24090d45b10a4fff9f61a5e6671817f
anahitahassan <anahitarhassan@gmail.com>
2021-04-16 16:29:52 -0700

    copy pasted new code

--- september10/sept10.java
+++ september10/sept10.java
@@ -1,3 +1,25 @@
-public class sept10 {
+//naming convention
+
+import java.util.Scanner;
+
+public class sept10
+{
+  public static void main(String[] args)
+  {
+      //typical rules: 
+           //start with lowercase letter (ex. int xyz)
+           //extrawords start uppercase (ex. int smallTable, bigFatHuman)
+           //no space allowed, so 2nd word uppercase is uppercase to be distinguished
+      //have to rules:
+           //never start with number
+           //allowed: letters, number, $, _ (ex. _tennis, _key_club$123)
+      //shortcuts (WILL BE ON QUIZ)
+         // i=i+1 ===> i++;
+         // i=i+7 ===> i+=7;
+         // i=i-7 ===> i-=7; also i*=7; i/=7; i%=7;
+      
+      
+    }
+    
     
 }
