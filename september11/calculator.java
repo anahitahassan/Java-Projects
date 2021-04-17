01b82198ea19a96a1be65bdc1189d5f621b3dae9
anahitahassan <anahitarhassan@gmail.com>
2021-04-17 11:54:59 -0700

    made tons of tiny changes

--- september11/calculator.java
+++ september11/calculator.java
@@ -6,87 +6,85 @@ public class calculator
     {
         Scanner scanner = new Scanner(System.in);
             
-        System.out.print("Press 1 if you are inputting integers, press 2 if you are inputting decimals");
+        System.out.print("Press 1 if you are inputting integers, press 2 if you are inputting decimals. ");
         int input = scanner.nextInt(); 
         
 
 
-        // INTEGERS ONLY
+        // OPTION 1: INTEGERS ONLY
         if ( input == 1 )
-            { 
-                System.out.print("Enter first integer: ");
-                int a = scanner.nextInt();
+        { 
+            System.out.print("Enter first integer: ");
+            int a = scanner.nextInt();
             
-                System.out.print("Enter second integer: ");
-                int b = scanner.nextInt();
+            System.out.print("Enter second integer: ");
+            int b = scanner.nextInt();
             
-                System.out.print("Enter an operator: press 1 for addition, 2 for subtraction, 3 for multiplication, 4 for division, and 5 for remainder ");
-                int operator = scanner.nextInt();
+            System.out.print("Enter an operator: press 1 for addition, 2 for subtraction, 3 for multiplication, 4 for division, and 5 for remainder. ");
+            int operator = scanner.nextInt();
                 
-                //operation
-                if ( operator == 1 )
-                {System.out.println( a + b );}  
+            //operation
+            if ( operator == 1 )
+            {System.out.println( a + b );}  
             
-                else if ( operator == 2 )
-                {System.out.println( a - b);}
+            else if ( operator == 2 )
+            {System.out.println( a - b);}
             
-                else if ( operator == 3 )
-                {System.out.println( a * b);} 
+            else if ( operator == 3 )
+            {System.out.println( a * b);} 
                 
-                else if ( operator == 4 )
-                {System.out.println( a / b);} 
+            else if ( operator == 4 )
+            {System.out.println( a / b);} 
         
-                else if ( operator == 5 )
-                {System.out.println( a % b);}
-            
-            }
-        
-        // DECIMALS ONLY
+            else if ( operator == 5 )
+            {System.out.println( a % b);}
+        }
+ 
+        // OPTION 2: DECIMALS ONLY
         else if ( input == 2 )
-            {
-                System.out.print("Enter first decimal number:");
-                double a = scanner.nextDouble();
+        {
+            System.out.print("Enter first decimal number: ");
+            double a = scanner.nextDouble();
             
-                System.out.print("Enter second decimal number:");
-                double b = scanner.nextDouble();
+            System.out.print("Enter second decimal number: ");
+            double b = scanner.nextDouble();
             
-                System.out.print("Enter an operator: press 1 for addition, 2 for subtraction, 3 for multiplication, and 4 for division");
-                int operator = scanner.nextInt();
+            System.out.print("Enter an operator: press 1 for addition, 2 for subtraction, 3 for multiplication, and 4 for division. ");
+            int operator = scanner.nextInt();
                 
-                //operation
-                if ( operator == 1 )
-                {System.out.println( a + b );}
+            //operation
+            if ( operator == 1 )
+            {System.out.println( a + b );}
                 
-                else if ( operator == 2 )
-                {System.out.println( a - b);}
+            else if ( operator == 2 )
+            {System.out.println( a - b);}
             
-                else if ( operator == 3 )
-                {System.out.println( a * b);}
+            else if ( operator == 3 )
+            {System.out.println( a * b);}
         
-                else if ( operator == 4 )
-                {System.out.println( a / b);}
-                    
-            }
+            else if ( operator == 4 )
+            {System.out.println( a / b);}     
+        }
 
-            // run program again?  
-            
-            System.out.print("Would you like to run the program again? Enter 1 for yes and 2 for no");
-            int run = scanner.nextInt();
+        // Run program again?  
+        System.out.print("Would you like to run the program again? Enter 1 for yes and 2 for no");
+        int run = scanner.nextInt();
+        
+        if ( run == 1 )
+        {
+            System.out.println("Would you like to run the program again? Your Answer: Yes");
 
-            if ( run == 1 )
-            
-            {System.out.println("Would you like to run the program again? Your Answer: Yes");
-                while ( run == 1)
-                { 
-                /// run program again
+            while ( run == 1)
+            { 
+                // Run program again
                 
                 Scanner scannerr = new Scanner(System.in);
-            
+
                 System.out.print("Press 1 if you are inputting integers, press 2 if you are inputting decimals");
                 int inputt = scannerr.nextInt(); 
 
                 if ( inputt == 1 )
-                    { 
+                { 
                     System.out.print("Enter first integer:");
                     int a = scannerr.nextInt();
             
@@ -96,7 +94,7 @@ public class calculator
                     System.out.print("Enter an operator: press 1 for addition, 2 for subtraction, 3 for multiplication, 4 for division, and 5 for remainder ");
                     int operator = scannerr.nextInt();
                 
-            //operation
+                    //operation
                     if ( operator == 1 )
                     {System.out.println( a + b );}  
             
@@ -112,10 +110,10 @@ public class calculator
                     else if ( operator == 5 )
                     {System.out.println( a % b);}
             
-                    }
+                }
         
-            else if ( inputt == 2 )
-                    {
+                else if ( inputt == 2 )
+                {
                     System.out.print("Enter first decimal number:");
                     double a = scannerr.nextDouble();
             
@@ -125,7 +123,7 @@ public class calculator
                     System.out.print("Enter an operator: press 1 for addition, 2 for subtraction, 3 for multiplication, and 4 for division");
                     int operator = scannerr.nextInt();
                 
-            //operation
+                    //operation
                     if ( operator == 1 )
                     {System.out.println( a + b );}
                 
@@ -138,21 +136,24 @@ public class calculator
                     else if ( operator == 4 )
                     {System.out.println( a / b);}
                     
-            }
-                
-                //end of re-run program
                 }
             
+                scannerr.close();
+                // end of re-run program
+                // end of while loop
             }
             
-            else if ( run == 2 )
+            // end of if statement
+        }
             
-            {System.out.println("Would you like to run the program again? Your Answer: No");}
-         
-            
+        else if ( run == 2 )
+        {System.out.println("Would you like to run the program again? Your Answer: No");}
+
         scanner.close();
+
+        // OK this would not have been an ideal way to re-run the program. 
+        // Granted I was a beginner when I made this calculator and I didn't know methods existed.
     }      
-      
 }       
    
          
