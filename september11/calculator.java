92b063b9a1732d7a1e151465ed969f78d5cfc9dc
anahitahassan <anahitarhassan@gmail.com>
2021-04-17 11:58:25 -0700

    comments

--- september11/calculator.java
+++ september11/calculator.java
@@ -151,6 +151,7 @@ public class calculator
 
         // OK this would not have been an ideal way to re-run the program. 
         // Granted I was a beginner when I made this calculator and I didn't know methods existed.
+        // The biggest issue about this is that it just copies previous code and you can only rerun the program once rip lol
     }      
 }       
    
