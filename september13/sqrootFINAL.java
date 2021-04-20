ca214165e632fee729068ffe617eacd3d8b860f8
anahitahassan <anahitarhassan@gmail.com>
2021-04-19 20:02:50 -0700

    changed file name

--- september13/sqrootFINAL.java
+++ september13/sqrootFINAL.java
@@ -1,3 +1,71 @@
-public class sqrootFINAL {
-    
+// THIS FILE WORKS!!!!!!!!!!!!!!!!
+
+import java.util.Scanner;
+
+public class sqrootFINAL
+{
+    public static int main (int num)
+    {
+        int sr = num / 2;
+        int temp;
+        do 
+        {
+            temp = sr; 
+            sr = ( temp + (num / temp)) / 2;
+        }
+        while (( temp - sr)!=0 ); 
+        return sr; 
+
+        // Example: say num = 81
+
+        // first round
+        // int sr = num / 2 => 81/2 = 40
+        // int temp = 40
+        // sr = (40 + (81/40))/2 => (40+2)/2 => 21
+        // while (40-21)=19= !=0 --> this statement is true still. 
+        
+        // Again...
+        // temp = 21
+        // sr = (21 + (81/21))/2 => (21+3)/2 => 12
+        // while (21-12)=9= !=0 --> this statement is true still. 
+        
+        // Again...
+        // temp = 12
+        // sr = (12 + (81/12))/2 => (12 + 6)/2 => 9
+        // while (12-9)=3= !=0 --> this statement is true still. 
+        
+        // Again...
+        // temp = 9
+        // sr = (9+(81/9))/2 => (9+9)/2 => 9 (Ayyyy look at that)
+        // while (9-9)!=0 --> this statement is finally false!
+
+        // so we return sr which is 9. 
+
+        // I don't have the slightest clue how that algorithm works though. 
+    }
+
+    public static void main(String[] args)
+    {
+        String yesno = "yes";
+        while (yesno == "yes")
+        {
+            System.out.print("Enter any number:");
+            Scanner scanner  = new Scanner(System.in);
+            int number = scanner.nextInt();
+            
+            System.out.println("Square root of " + number + " is: " + main(number));
+
+            System.out.println("Print 'yes' if you would like to re-run program and 'no' if you would not like to re-run.");
+            String user = scanner.next();
+
+            // before I used (user == "no"), and it didn't work
+            // but .equals works. 
+            if (user.equals("no"))
+            {
+                yesno = "no";
+                scanner.close();
+                System.out.println("End of program");
+            }
+        }
+    }
 }
