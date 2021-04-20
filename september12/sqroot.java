2f8b255834ae88dd0fe2d6c96a60bb12c0709448
anahitahassan <anahitarhassan@gmail.com>
2021-04-19 19:34:45 -0700

    copied and pasted

--- september12/sqroot.java
+++ september12/sqroot.java
@@ -1,3 +1,36 @@
-public class sqroot {
+//square root project due next thurs 9/19/19
+
+import java.util.Scanner;
+
+public class srproj 
+{
+  public static int main (int num)
+  {
     
+    int a;
+      
+    int sr = num / 2;
+    
+    do 
+    {
+      temp = sr; 
+      sr = ( temp + (num / temp)) / 2;
+      
+    }
+     while (( temp - sr)!=0 ); 
+     return sr; 
+  }
+  
+  public static void main(String[] args)
+  {
+    
+    System.out.print("Enter any number:");
+    Scanner scanner  = new Scanner(System.in);
+    int number = scanner.nextInt();
+    scanner.close();
+    
+    System.out.println("Square root of " + number + " is: " + main(number));
+    
+  }
+  
 }
