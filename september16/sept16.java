0db04b577686cc51419c11cb465ebd3c6318e688
anahitahassan <anahitarhassan@gmail.com>
2021-04-21 19:27:25 -0700

    13

--- september16/sept16.java
+++ september16/sept16.java
@@ -28,7 +28,7 @@ public class sept16
         // of the first loop it is on. (sounds complicated but it really isn't)
         
         // let's run through it. say the user said a=4. 
-        
+
         // i = 1 --> j = 1 --> print * (and since j=1, this only happens once)
         // new line, end of first (/4) iteration of 1st for loop
         // i = 2 --> j = 1 --> print * (and j will be 1, then 2 since i = 2, so ** is printed)
@@ -37,8 +37,5 @@ public class sept16
         // new line, end of third (/4) iteration of 1st for loop
         // i = 4 --> j = 1 --> print * (and j will be 1, then 2, then 3, then 4 since i = 4, so **** is printed)
         // new line, end of fourth (/4) iteration of 1st for loop, loop is complete. 
-
-
-
     }
 }
