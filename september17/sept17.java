17044a9adca3b499171762823482465c534bedec
anahitahassan <anahitarhassan@gmail.com>
2021-04-21 19:31:03 -0700

    109

--- september17/sept17.java
+++ september17/sept17.java
@@ -2,19 +2,18 @@ import java.util.*;
 
 public class sept17
 {
-  public static void main(String[] args)
-  {
-    Scanner project = new Scanner(System.in);
-    System.out.println("Please enter a number.");
-    
-    int i;
-    int j = 1;
-    int a = project.nextInt(); 
-    for (i = a; i > 1; i--) {
-      j = j * i;
+    public static void main(String[] args)
+    {
+        Scanner project = new Scanner(System.in);
+        System.out.println("Please enter a number.");
+        
+        int i;
+        int j = 1;
+        int a = project.nextInt(); 
+        for (i = a; i > 1; i--) {
+        j = j * i;
+        }
+        
+        System.out.println(j);
     }
-     
-    System.out.println(j);
-  }
- 
 }
