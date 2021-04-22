937d1c6ae823f28889ffb541a02aaefbe20ac0fe
anahitahassan <anahitarhassan@gmail.com>
2021-04-21 19:16:10 -0700

    3

---
+++ september16/sept16.java
@@ -0,0 +1,10 @@
+// idk what this file is for lmfao there is barely any code here
+import java.util.*;
+
+public class Sept16
+{
+  public static double main(String[] args)
+  {
+    Scanner iDunno = new Scanner(System.in); 
+  }
+}
