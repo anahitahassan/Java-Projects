55412efa3cc52170d0e8d0c19fe42a47542d5e89
anahitahassan <anahitarhassan@gmail.com>
2021-04-21 19:24:23 -0700

    10

--- september16/sept16.java
+++ september16/sept16.java
@@ -28,8 +28,9 @@ public class sept16
         
         // let's run through it. say the user said a=4. 
         // i = 1 --> j = 1 --> print * (and since j=1, this only happens once)
-        // new line, end of first iteration of 1st for loop
+        // new line, end of first (/4) iteration of 1st for loop
         // i = 2 --> j = 1 --> print * (and j will be 1, then 2 since i = 1, so ** is printed)
+        // new line, end of second (/4) iteration of 1st for loop
 
 
 
