a6a38325c997a96d89f4ecc1e58fb4ecbb85e32b
anahitahassan <anahitarhassan@gmail.com>
2021-04-21 19:29:55 -0700

    1

---
+++ september17/sept17.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+
+public class sept17
+{
+  public static void main(String[] args)
+  {
+   
+    Scanner project = new Scanner(System.in);
+    System.out.println("Please enter a number.");
+    
+    int i;
+    int j = 1;
+    int a = project.nextInt(); 
+    for (i = a; i > 1; i--) {
+      j = j * i;
+    }
+     
+    System.out.println(j);
+  }
+ 
+}
