d52f74bf7573227cff217978497411064e75459e
anahitahassan <anahitarhassan@gmail.com>
2021-04-25 16:42:49 -0700

    2

--- september20/program1.java
+++ september20/program1.java
@@ -1,3 +1,22 @@
-public class program1 {
-    
+import java.util.*;
+
+public class program1
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter your 1st number.");
+        int a = input.nextInt();
+        
+        System.out.println("Please enter your 2nd number.");
+        int b = input.nextInt();
+        
+        System.out.println("Please enter your 3rd number.");
+        int c = input.nextInt();
+        
+        System.out.println((-b + (Math.sqrt((b*b)-4*a*c)))/2*a);
+        System.out.println((-b - (Math.sqrt((b*b)-4*a*c)))/2*a);
+        
+        input.close();
+    }
 }
