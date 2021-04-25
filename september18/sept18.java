ca15faa45d3556c303dcafffa9ef6839dd820a98
anahitahassan <anahitarhassan@gmail.com>
2021-04-24 17:54:42 -0700

    12

--- september18/sept18.java
+++ september18/sept18.java
@@ -11,12 +11,17 @@ public class sept18
         int j = 1;
         int v = input.nextInt();
         input.close();
-        
+
         for (i = v; i > 1; i++) 
         {
             j = 3 * i;
         }
         
         System.out.println(j);
+
+        // i is set equal to the input from the user, and as long as i remains larger than 1, 
+        // j, which is initally 1, will be multiplied by 3
+        // this will happen an infinite number of times because i is incremented
+
     }
 }
