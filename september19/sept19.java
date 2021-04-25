452dde21358aa546578f66b1d094e0475a3307fb
anahitahassan <anahitarhassan@gmail.com>
2021-04-24 18:15:51 -0700

    18

--- september19/sept19.java
+++ september19/sept19.java
@@ -11,16 +11,16 @@ public class sept19
 
        for (i = 1; i <= 100; i++)         
        {        
-            int counter=0;    
-            for(num =i; num>=1; num--)
+            int counter = 0;    
+            for(num = i; num >= 1; num--)
             {
-                if(i%num==0)
+                if(i % num == 0)
                 {
                     counter = counter + 1;
                 }
             }
 
-            if (counter ==2)
+            if (counter == 2)
             {
                 //Appended the Prime number to the String
                 primeNumbers = primeNumbers + i + " ";
