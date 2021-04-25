1cde961e2bd5d2282db4e1fe0cf27a685c9d552d
anahitahassan <anahitarhassan@gmail.com>
2021-04-24 17:53:20 -0700

    11

--- september18/sept18.java
+++ september18/sept18.java
@@ -1,14 +1,20 @@
+import java.util.*;
+
 public class sept18 
 {
     public static void main(String[] args)
     {
+        Scanner input = new Scanner(System.in);
         System.out.println("Please enter an upper bound.");
-    
+
         int i;
         int j = 1;
         int v = input.nextInt();
-        for (i = v; i > 1; i++) {
-        j = 3 * i;
+        input.close();
+        
+        for (i = v; i > 1; i++) 
+        {
+            j = 3 * i;
         }
         
         System.out.println(j);
