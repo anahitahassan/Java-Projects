57432199d6bbb103b95b9605525162dc82cbeccf
anahitahassan <anahitarhassan@gmail.com>
2021-04-24 17:41:03 -0700

    3

--- september17/sept17pt2.java
+++ september17/sept17pt2.java
@@ -3,22 +3,24 @@ import java.util.*;
 
 public class sept17pt2
 {
-  public static void main(String[] args)
-  {
-   
-    Scanner input = new Scanner(System.in);
-    System.out.println("Please enter a number.");
-    
-    int a = input.nextInt();
-    input.nextLine();
-    
-    int answer = a;
-    a--;
-    while( a > 0 )
+    public static void main(String[] args)
     {
-      answer *= a;
-      a--;
-      
-      
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter a number.");
+        
+        int a = input.nextInt();
+        input.nextLine();
+        
+        int answer = a;
+        a--;
+        
+        while( a > 0 )
+        {
+            answer *= a;
+            a--;
+        }
+        
+        System.out.println(answer);
+        input.close();
     }
-    System.out.println(answer);
+}
