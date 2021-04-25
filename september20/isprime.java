26b6c9b1a1febe8da8bb3ce7eabf689c8e8f176e
anahitahassan <anahitarhassan@gmail.com>
2021-04-25 16:46:03 -0700

    6

--- september20/isprime.java
+++ september20/isprime.java
@@ -1,37 +1,35 @@
-
 import java.util.*;
 
 public class isprime
 {  
-  public static void main (String[] args)
-  {
-    Scanner scanner = new Scanner(System.in);
-    System.out.println("Enter the number");
-    int a = scanner.nextInt();
-         
-    int i;
-    int r = 0;
-    boolean numberIsPrime = true;
+    public static void main (String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int a = scanner.nextInt();
+            
+        int i;
+        int r = 0;
+        boolean numberIsPrime = true;
+            
+        for (i = 2; i <= (a / 2); i++)
+        {
+            r = a % i;
+            if( r == 0)
+            {
+                numberIsPrime = false;
+            }
+        }
         
-    for (i = 2; i <= (a / 2); i++)
-      {
-        r = a % i;
-          if( r == 0)
-           {
-             numberIsPrime = false;
-           }
-      }
-    
-     if (numberIsPrime == true)
-      {
-         System.out.println (a + " is a prime.");
-      }
-     
-     else
-      {
-         System.out.println (a + " is not a prime.");
-      }      
-         
-  }
-  
+        if (numberIsPrime == true)
+        {
+            System.out.println (a + " is a prime.");
+        }
+        
+        else
+        {
+            System.out.println (a + " is not a prime.");
+        }      
+        scanner.close(); 
+    }
 }
