78a220336e51f97f355664a0bd744e95298b4feb
anahitahassan <anahitarhassan@gmail.com>
2021-04-24 17:51:53 -0700

    10

---
+++ september18/sept18.java
@@ -0,0 +1,16 @@
+public class sept18 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Please enter an upper bound.");
+    
+        int i;
+        int j = 1;
+        int v = input.nextInt();
+        for (i = v; i > 1; i++) {
+        j = 3 * i;
+        }
+        
+        System.out.println(j);
+    }
+}
