7ff9f8bc300fe98314d734299a037fba626508e0
anahitahassan <anahitarhassan@gmail.com>
2021-04-24 17:48:27 -0700

    7

--- september18/sept18.java
+++ september18/sept18.java
@@ -1,14 +1,9 @@
-import java.util.*;
-
 public class sept18
 {
-  public static void main(String[] args)
-  {
-    
-    double x = 2;
-    double y = Math.sqrt(x);
-    System.out.println(y);
-    
-  }
-  
+    public static void main(String[] args)
+    {
+        double x = 2;
+        double y = Math.sqrt(x);
+        System.out.println(y);
+    } 
 }
