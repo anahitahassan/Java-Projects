d7fa6ff511064940d7fa4517f9c91de6e8a88ae5
anahitahassan <anahitarhassan@gmail.com>
2021-04-28 15:39:57 -0700

    new file

---
+++ september24/updatedsqrt.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+    
+public class squareRootCalculator2
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in); 
+        System.out.println("Enter a number ");
+        int userInput = scanner.nextInt();
+              
+        if (userInput <= 3)
+        {
+            System.out.println ("Square root is root  " + userInput);
+        }
+        else
+        {
+            int i = 2;
+            int input = userInput;
+            int outside = 1;
+            int inside = 0;
+            String printStatement = "";
+
+            while ( (i * i) <= input )
+            {
+                int quotient = input / (i * i);
+               
+                if (input == (i * i)) 
+                {
+                    outside = outside * i;
+                } 
+
+                if (input % (i * i) == 0)
+                {
+
+                    outside = outside * i; 
+                    i = 1; 
+                    input = quotient;                
+                }
+                i++;
+            }
+
+            if (input % (i * i) > 0) 
+            {
+                inside = input;
+            }
+            
+            if (inside == 0)
+            {
+                printStatement = "Square root is " + outside;
+            }
+            else
+            {
+                if (outside == 1)
+                    printStatement = "Square root is root  " + inside;
+                else
+                    printStatement = "Square root is " + outside + "  root  " + inside;
+                    
+            }
+
+            System.out.println (printStatement); 
+            
+        }
+    }    
+}
