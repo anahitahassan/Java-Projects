52ffd63bd44de051e06d20b62f6645878fe4dab9
anahitahassan <anahitarhassan@gmail.com>
2021-05-05 13:13:11 -0700

    new filen ame

---
+++ september26/intro-to-methods.java
@@ -0,0 +1,22 @@
+
+import java.util.*;
+
+public class introToMethods
+{
+    public static void main(String[] args)
+    {
+        counter();
+        counter();
+    }
+    public static void counter()
+    {
+        int x = 1;
+        while( x < 21)
+        {
+            System.out.print(x + ", ");
+            x++;
+        }
+        System.out.println();
+    }
+    
+}
