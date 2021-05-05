12b8ac734b781acb1a9875e8256eaad8d231f813
anahitahassan <anahitarhassan@gmail.com>
2021-05-05 13:08:33 -0700

    new comment

--- september25/division.java
+++ september25/division.java
@@ -5,15 +5,15 @@ public class division
     public static void main (String[] args)
     {
         Scanner scanner = new Scanner(System.in); 
-        System.out.println("Enter the 1st number");
+        System.out.println("Enter the 1st number: ");
         int a = scanner.nextInt();
       
-        System.out.println("Enter the 2nd number");
+        System.out.println("Enter the 2nd number: ");
         int b = scanner.nextInt();
         
-        int s = a / b;
-        int r = a % b;
-        int q = (r * 10)/ b;
+        int s = a / b; // getting the "main" number
+        int r = a % b; // getting the remainder (which will be a whole number)
+        int q = (r * 10)/ b; // in 'mixed' fractions the remainder is divided by 
         
         if (r == 0)
         {
