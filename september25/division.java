e720947000db6575d5cedc7d161a7c3f25a968be
anahitahassan <anahitarhassan@gmail.com>
2021-05-05 13:06:37 -0700

    new cfile

---
+++ september25/division.java
@@ -0,0 +1,34 @@
+/*
+ * @author: Anahita Hassan
+ */
+import java.util.Scanner;
+
+public class division
+{
+    public static void main (String[] args)
+    {
+        Scanner scanner = new Scanner(System.in); 
+        System.out.println("Enter the 1st number");
+        int a = scanner.nextInt();
+      
+        System.out.println("Enter the 2nd number");
+        int b = scanner.nextInt();
+        
+        int s = a / b;
+        int r = a % b;
+        int q = (r * 10)/ b;
+        
+        if (r == 0)
+        {
+            System.out.println("The answer is " + a/b);
+        } 
+        
+        else
+        {
+            System.out.println("The answer is " + s + "." + q);
+        }
+        
+        scanner.close();
+    }
+}
+       
