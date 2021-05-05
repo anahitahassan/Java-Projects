d950c45a9ea28a6bf51720414dcb0374b8ab6691
Anahita Hassan <67247100+anahitahassan@users.noreply.github.com>
2021-05-04 19:52:27 -0700

    don't need import

--- september18/mathsqrt.java
+++ september18/mathsqrt.java
@@ -5,5 +5,6 @@ public class mathsqrt
         double x = 15;
         double y = Math.sqrt(x);
         System.out.println(y);
+        // I don't believe you need to import anything to use math.sqrt ???
     } 
 }
