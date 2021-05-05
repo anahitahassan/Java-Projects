f02497cba7251b5a1d4ff0290a3714e6068e9f73
anahitahassan <anahitarhassan@gmail.com>
2021-05-05 13:10:57 -0700

    xplaination

--- september25/division.java
+++ september25/division.java
@@ -16,6 +16,12 @@ public class division
         int q = (r * 10)/ b; // in 'mixed' fractions the remainder is divided by the original divider
         // but we also need to multiply by 10 because we want whole number, not decimal. 
 
+        // say we want 7/3
+        // s = 2
+        // r = 1
+        // fraction part of 'mixed' fraction is 1/2 but we don't want 0.5
+        // we want 5, not 0.5, so we *10
+
         if (r == 0)
         {
             System.out.println("The answer is " + a/b);
