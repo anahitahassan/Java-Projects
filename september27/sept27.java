342202426d8a0ec04b00064d21e8e5b874409282
anahitahassan <anahitarhassan@gmail.com>
2021-05-16 15:19:32 -0700

    new

--- september27/sept27.java
+++ september27/sept27.java
@@ -1,3 +1,33 @@
-public class sept27 {
-    
+/*
+ * @author: Anahita Hassan
+ */
+import java.util.Scanner;
+
+public class sept27
+{
+    public static void main (String[] args)
+    {
+        Scanner scanner = new Scanner(System.in); 
+        System.out.println("Enter the 1st number");
+        int a = scanner.nextInt();
+      
+        System.out.println("Enter the 2nd number");
+        int b = scanner.nextInt();
+        
+        int s = a / b;
+        int r = a % b;
+        int q = (r * 10)/ b;
+        
+        if (r == 0)
+        {
+            System.out.println("The answer is " + a/b);
+        } 
+        
+        else
+        {
+            System.out.println("The answer is " + s + "." + q);
+        }
+                  
+    }
 }
+       
