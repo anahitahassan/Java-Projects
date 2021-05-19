5e089a40398e6412416bfd32ffc251f2eb615aa1
anahitahassan <anahitarhassan@gmail.com>
2021-05-19 15:42:03 -0700

    comment for second example

--- september30/sept30.java
+++ september30/sept30.java
@@ -7,7 +7,7 @@ public class sept30
         int z = add(x,y);
         System.out.println(z); // since vars representing 3 and 9 are passed, 12 is printed. 
 
-        System.out.println(add(100,100));
+        System.out.println(add(100,100)); // since 100 and 100 are passed directly into the method, 200 is printed. 
     }
 
     public static int add(int a, int b)
