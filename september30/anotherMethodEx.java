c70afd093fe5a63060f0c05be35ecbe98067db43
anahitahassan <anahitarhassan@gmail.com>
2021-05-19 15:49:38 -0700

    moved comments

--- september30/anotherMethodEx.java
+++ september30/anotherMethodEx.java
@@ -4,15 +4,27 @@ public class anotherMethodEx
     {
         int i = 2;
         doThis(i); 
-        System.out.println(i); 
+        System.out.println(doThis(i)); 
+        // so i is changed in the method, and we see that when it is printed out. 
+        // even after we call that method, i doesn't change. 
+
+        int a = 700; 
+        System.out.println(doThisDifferently(a));  
         
     }
 
     // here, it says void after public static, meaning nothing is returned. 
-    public static void doThis(int i)
+    public static void doThis(int j)
     {
-        i = 3;
-        System.out.println("method:" + i);
+        j = 3;
+        System.out.println("method:" + j);
     }
+    public static int doThisDifferently(int b)
+    {
+        b = 800;
+        return b;
+    }
+
+
 }
 
