7a657fa340fee801df30e567fd8a7853cc531951
anahitahassan <anahitarhassan@gmail.com>
2021-05-19 15:44:18 -0700

    new comment explaining concept

--- september30/anotherMethodEx.java
+++ september30/anotherMethodEx.java
@@ -1,18 +1,15 @@
 public class anotherMethodEx
 {
-  public static void main(String[] args)
-  {
-      int i = 2;
-      doThis(i);
-      System.out.println(i);
-      
-  }
-  
-  public static void doThis(int i)
-  {
-      i = 3;
-      
-  }
-  
+    public static void main(String[] args)
+    {
+        int i = 2;
+        doThis(i);
+        System.out.println(i);
+        
+    }
+    public static void doThis(int i)
+    {
+        i = 3;
+    }
 }
 
--- september30/sept30.java
+++ september30/sept30.java
@@ -10,6 +10,8 @@ public class sept30
         System.out.println(add(100,100)); // since 100 and 100 are passed directly into the method, 200 is printed. 
     }
 
+    // notice how it says int after public static
+    // this indicates that the variable returned will be an int. 
     public static int add(int a, int b)
     {
         int answer = a + b;
