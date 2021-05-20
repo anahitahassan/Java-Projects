fddcabbc3c97c0924ed70814208ce2c4b63e57c7
anahitahassan <anahitarhassan@gmail.com>
2021-05-20 08:07:33 -0700

    created tttboard

---
+++ september30/tttboard.java
@@ -0,0 +1,34 @@
+
+import java.util.*;
+
+public class tttboard
+{
+    public static void main(String[] args)
+    { 
+        System.out.println("Let's play tic-tac-toe!");
+        counter();
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Player 1 (X): enter the designated letter for the location on the board");
+        int aa = scanner.nextInt();
+        
+        counter(); 
+        
+     
+
+        
+
+    }
+    
+    public static void counter()
+    {
+       System.out.println(" a | b | c "); 
+       System.out.println("---|---|---"); 
+       System.out.println(" d | e | f "); 
+       System.out.println("---|---|---"); 
+       System.out.println(" g | h | i "); 
+    }
+    
+}
+
+//tic tac toe project due next week?
