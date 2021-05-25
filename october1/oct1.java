b6ae7e728949beee9bf688894adc7fedbee6c7e5
anahitahassan <anahitarhassan@gmail.com>
2021-05-24 18:01:24 -0700

    fixed spaces

--- october1/oct1.java
+++ october1/oct1.java
@@ -1,24 +1,13 @@
-
-import java.util.*;
+//import java.util.*;
 
 public class oct1
 {
     public static void main(String[] args)
     {
-      //math.random: [0, 1)
-      
-      
-        
-        
-        
-      double x = Math.random();
-      System.out.println(10*x);
-      
+        //math.random: [0, 1)
+            
+        double x = Math.random();
+        System.out.println(10*x);
 
-        
-        
-        
-        
     }
-    
 }
