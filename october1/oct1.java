3ccbcc35d8763be3373d14e0823c561157a910aa
anahitahassan <anahitarhassan@gmail.com>
2021-05-24 17:59:56 -0700

    new file

---
+++ october1/oct1.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+
+public class 8ball
+{
+    public static void main(String[] args)
+    {
+      //math.random: [0, 1)
+      
+      
+        
+        
+        
+      double x = Math.random();
+      System.out.println(10*x);
+      
+
+        
+        
+        
+        
+    }
+    
+}
