fa74ffa6ee577aa4765d554ee7f4b1a401431548
Anahita Hassan <67247100+anahitahassan@users.noreply.github.com>
2021-05-25 15:44:41 -0700

    comment

--- october1/oct1.java
+++ october1/oct1.java
@@ -2,6 +2,7 @@ public class oct1
 {
     public static void main(String[] args)
     {
+        // you don't need to import anything in order to use Math.random() function. 
         //math.random: [0, 1)
             
         double x = Math.random();
