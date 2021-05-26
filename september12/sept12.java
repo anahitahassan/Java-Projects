c2f4e5e1193115952043fe278444b6a16ff870e1
Anahita Hassan <67247100+anahitahassan@users.noreply.github.com>
2021-05-26 12:11:09 -0700

    deleted a line

--- september12/sept12.java
+++ september12/sept12.java
@@ -8,7 +8,6 @@ public class sept12
         System.out.println("hello");
     }
     // printed when i = 3, 4, 5, ... 8, 9. 
-    // 7 times
     // useful for when you know exactly how many times it should run
       
     //QUIZ NEXT WEDNESDAY ;;; THIS WONT BE ON QUIZ
