f6b899622a47a981399866ac8c87338191f8f00e
anahitahassan <anahitarhassan@gmail.com>
2021-07-02 14:17:00 -0700

    new file

---
+++ october2/scratchwork.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class scratchwork
+{
+    public static void main(String[] args)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter numerator of the first number");
+        int a = scanner.nextInt();
+        
+        System.out.println("Enter denominator of the first number");
+        int b = scanner.nextInt();
+        
+        System.out.println("Enter numerator of the second number");
+        int c = scanner.nextInt();
+        
+        System.out.println("Enter denominator of the second number");
+        int d = scanner.nextInt();
+        
+        System.out.println("Pick an operation: enter 1 for addition, 2 for subtraction, 3 for multiplication, 4 for division");
+        int op = scanner.nextInt();
+        
+        if ( op == 1 )
+            //only addition
+        { 
+            if (b == d) 
+                //same denominator, simply adds the numerators
+            { 
+                String name = ((a+c)+ "/" + b); 
+            }
+            
+        }
+        
+        System.out.println(name);
+        
+    }
+    
+}
