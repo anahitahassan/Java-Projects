378640abbe884dca1096d304a836e6a6a502bdfe
anahitahassan <anahitarhassan@gmail.com>
2021-07-02 14:12:51 -0700

    new october2 file

---
+++ october2/oct2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class oct2
+{
+    public static void main(String[] args)
+    {
+      String x = "jack";
+      String y = "jack";
+      
+      if ( x .equals(y) )
+      {
+          System.out.println("hello");
+      }
+      
+      //int: " == "
+      //String: " x .equals(y) "
+        
+        
+    }
+    
+    
+}
