f28afcf60744963362e372bab223a9852330d22e
anahitahassan <anahitarhassan@gmail.com>
2021-07-02 14:14:31 -0700

    new commit

--- october2/oct2.java
+++ october2/oct2.java
@@ -1,20 +1,16 @@
 public class oct2
 {
-    public static void main(String[] args)
+  public static void main(String[] args)
+  {
+    String x = "jack";
+    String y = "jack";
+      
+    if ( x .equals(y) )
     {
-      String x = "jack";
-      String y = "jack";
-      
-      if ( x .equals(y) )
-      {
-          System.out.println("hello");
-      }
-      
-      //int: " == "
-      //String: " x .equals(y) "
-        
-        
+      System.out.println("hello");
     }
-    
-    
+      
+    //int: " == "
+    //String: " x .equals(y) "          
+  }
 }
