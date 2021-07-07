5a5aa5b8f10b22a959b976fb43a4c9b9460bf515
anahitahassan <anahitarhassan@gmail.com>
2021-07-06 17:42:30 -0700

    new file

---
+++ october5/TTT.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class TTT {
+    public static int row, col;
+    public static Scanner scanner = new Scanner(System.in);
+    public string[][] board = new string [3][3];
+    public static string turn = "X"; 
+    
+    public static void main(String[] args){
+        
+    }
+    
+}
