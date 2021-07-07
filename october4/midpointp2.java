7d173bc5f5ef2e22e706627092366c55b81deba6
anahitahassan <anahitarhassan@gmail.com>
2021-07-06 17:39:20 -0700

    created new file

---
+++ october4/midpointp2.java
@@ -0,0 +1,32 @@
+
+import java.util.*;
+
+public class midpointp2
+{
+    public static void main(String[] args)
+    {
+      boolean midpoint = Midpoint (-1, 4, 5, -6);
+      System.out.println(midpoint);     
+    }
+    
+    public static boolean Midpoint(int x1, int x2, int y1, int y2)
+    {
+        double x = (double)(x1 + x2)/2;
+        double y = (double)(y1 + y2)/2;
+        System.out.println("(" + x + "," + y + ")"); 
+        
+        if ( x % 1 == 0 && y % 1 == 0 && x >= 0 && y >= 0 )
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
+    } 
+   
+}
+        
+        
+        
