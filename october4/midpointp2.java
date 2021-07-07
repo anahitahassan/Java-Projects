0e9ed5c70bc35e6d2124b10bccbef30c780d5561
anahitahassan <anahitarhassan@gmail.com>
2021-07-06 17:40:17 -0700

    index

--- october4/midpointp2.java
+++ october4/midpointp2.java
@@ -2,8 +2,8 @@ public class midpointp2
 {
     public static void main(String[] args)
     {
-      boolean midpoint = Midpoint (-1, 4, 5, -6);
-      System.out.println(midpoint);     
+        boolean midpoint = Midpoint (-1, 4, 5, -6);
+        System.out.println(midpoint);     
     }
     
     public static boolean Midpoint(int x1, int x2, int y1, int y2)
