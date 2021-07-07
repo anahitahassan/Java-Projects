e8aa17cfc3550c1e7504b4e07a20792a2f3eb04c
anahitahassan <anahitarhassan@gmail.com>
2021-07-06 17:33:37 -0700

    made new file and folder

---
+++ october4/oct4.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+
+public class oct4
+{
+    public static void main(String[] args)
+    {
+        int a = 6;
+        int b = 9;
+        int i = 2; 
+        
+        if ( a / b == 1) {
+            System.out.println("1"); }
+            
+        if (a % i == 0 && b % i == 0){
+            System.out.println((a/i)/(b/i)); } 
+        i++;
+        
+
+  
+     
+    }
+}
