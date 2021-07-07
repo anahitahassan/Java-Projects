45943e7c5bb4d03c1abca4ad519ccfd9d3e6f429
anahitahassan <anahitarhassan@gmail.com>
2021-07-06 17:47:43 -0700

    explaining static

--- october5/TTT.java
+++ october5/TTT.java
@@ -1,12 +1,22 @@
 import java.util.*;
 
 public class TTT {
+
+    /* STATIC:
+    *  int is a datatype for a variable storing integer values. 
+    *  static int is a variable storing integer values which is declared static. 
+    *  If we declare a variable as static, it exists till the end of the program once initialized.
+    */ 
     public static int row, col;
+
     public static Scanner scanner = new Scanner(System.in);
+
     public String[][] board = new String [3][3];
+
     public static String turn = "X"; 
     
-    public static void main(String[] args){
+    public static void main(String[] args)
+    {
         
     }
     
