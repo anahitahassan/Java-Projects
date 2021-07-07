65f2d7db763f45ae5285ed52b38926295dde8297
anahitahassan <anahitarhassan@gmail.com>
2021-07-06 17:45:28 -0700

    made string uppercase

--- october5/TTT.java
+++ october5/TTT.java
@@ -3,8 +3,8 @@ import java.util.*;
 public class TTT {
     public static int row, col;
     public static Scanner scanner = new Scanner(System.in);
-    public string[][] board = new string [3][3];
-    public static string turn = "X"; 
+    public String[][] board = new String [3][3];
+    public static String turn = "X"; 
     
     public static void main(String[] args){
         
