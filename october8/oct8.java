5780ffea37addf64623996b8e65f1147e615917f
anahitahassan <anahitarhassan@gmail.com>
2021-07-15 21:55:50 -0700

    new

--- october8/oct8.java
+++ october8/oct8.java
@@ -2,30 +2,28 @@ public class oct8
 {
     public static void main(String[] args )
     {
-       int a = 5;
-       int b = 6;
-       System.out.println(add(a, b));
-       System.out.println(mult(a, b));
-       
+        int a = 5;
+        int b = 6;
+        System.out.println(add(a, b));
+        System.out.println(mult(a, b));
     }
    
     public static int add(int a, int b)
     {
-       int c = a + b; 
-       return c; 
+        int c = a + b; 
+        return c; 
     } 
     
     public static int mult(int a, int b)
     {
-       int i = a;
-       int j;
-       int d = 0;
-       for( j = 1 ;  j <= b ; j++) {
-           d = i + d;}
-       
-
-       return d;
-        
+        int i = a;
+        int j;
+        int d = 0;
+        for( j = 1 ; j <= b ; j++) 
+        {
+            d = i + d;
+        }
+        return d;
     }
 
 }
