a89929f1b03a89c7f95b033ba818efc5255f297c
anahitahassan <anahitarhassan@gmail.com>
2021-07-15 21:59:27 -0700

    new

--- october10/methodsreview.java
+++ october10/methodsreview.java
@@ -6,14 +6,14 @@ public class methodsreview
        int top = 5;
        int bottom = 6;
        
-       System.out.println(stuff(top,bottom));
+       System.out.println( stuff(top,bottom) );
     }
     // quiz next wednesday/thursday   10/16 10/17  
     
     //b automatically becomes the int a, a becomes the int b, etc...    
-    public static int stuff(int top, int bottom)
+    public static int stuff(int a, int b)
     {
-       int y = (top+bottom); 
+       int y = a + b; 
        return y; 
     }
 }
