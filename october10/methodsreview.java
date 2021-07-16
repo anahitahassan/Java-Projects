6764cc087eedb331d033866dfe1ec24d7510b807
anahitahassan <anahitarhassan@gmail.com>
2021-07-15 21:57:46 -0700

    new folder

---
+++ october10/methodsreview.java
@@ -0,0 +1,45 @@
+//methods and returns
+import java.util.*;
+
+public class methodsreview 
+{
+    public static void main( String[] args )
+    {
+       int top = 5;
+       int bottom = 6;
+       
+       System.out.println(stuff(top,bottom));
+    }
+// quiz next wednesday/thursday   10/16 10/17  
+    
+    //b automatically becomes the int a, a becomes the int b, etc...    
+    public static int stuff(int top, int bottom)
+    {
+       int y = (top+bottom); 
+       return y; 
+    }
+}
+        
+        
+// public static void main( String[] args ) 
+//    {
+  //     int b = 5;
+    //   int a = 6;
+      // String c = "hello";
+       //System.out.println(stuff(b,9, c, true));
+    
+// quiz next wednesday/thursday   10/16 10/17  
+    
+ //   public static int stuff(int a, int b, String c, boolean d )
+ //   {
+  //     System.out.println(a); 
+  //     return b; 
+  //  }   
+    
+    
+        
+        
+    
+    
+
+      
