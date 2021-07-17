4b494811afeded75ab8c959ea12156a6f1a267c5
anahitahassan <anahitarhassan@gmail.com>
2021-07-16 19:41:48 -0700

    new

--- october12/oct12.java
+++ october12/oct12.java
@@ -2,8 +2,8 @@ import java.util.*;
 
 public class oct12 {
 
-    public static void main(String[] args){
-
+    public static void main(String[] args)
+    {
         //Enter two number whose GCD needs to be calculated.      
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter first number");
@@ -11,11 +11,16 @@ public class oct12 {
         System.out.println("Please enter second number");
         int n2 = scanner.nextInt();
        
-        while (n1 != 0 && n2 != 0){
+        while (n1 != 0 && n2 != 0)
+        {
             if(n1 > n2)
-            n1 = n1 % n2;
+            {
+                n1 = n1 % n2;
+            }
             else
-            n2 = n2 % n1;
+            {
+                n2 = n2 % n1;
+            }
         }
        
         scanner.close();
