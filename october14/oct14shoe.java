afdf44dc38e094a6f62cad4c5191d157d60b3984
Anahita Hassan <anahitarhassan@gmail.com>
2021-07-21 19:58:12 -0700

    new

---
+++ october14/oct14shoe.java
@@ -0,0 +1,35 @@
+/*
+import java.util.*;
+
+public class oct14shoe
+{
+    public static void main(String[] args) 
+    {  
+       
+       // new shoe(), since its empty is will use the default settings (size 7 color white)
+       
+       // Example 1
+       shoe Ana = new shoe();
+       
+       System.out.println(Ana.getSize());
+       System.out.println(Ana.getColor());
+       Ana.setColor( "green" );
+       System.out.println(Ana.getColor());
+       
+       
+       // Example 2
+       shoe Natalie = new shoe( 6, "black");
+       System.out.println(Natalie.getColor());
+       
+       // Example 3
+       int x = 3;
+       String y = "purple";
+       shoe bob = new shoe ( x,y );
+       System.out.println(bob.getSize());
+       
+ 
+       
+    }
+    
+}
+*/
